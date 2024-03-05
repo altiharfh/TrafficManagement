65a3314c6c72232fe4d8f3b8f17e08420fcf1f0e
Altinger Harald <altinger.harald@gmail.com>
2024-03-05 12:26:58 +0100

    using black code formater python -m black .\main.py see https://github.com/psf/black

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 __doc__ = "sample file for a main methode"
 
-#import system libs
+# import system libs
 import argparse
 import sys
 
-#own modules
+# own modules
 from examples.plot_concarve_hul import plot_concarve_
 
-#something to start
+# something to start
 print("Hello world\n")
 
-#setup arg parser
+# setup arg parser
 arg_parser_ = argparse.ArgumentParser(description="Process some integers.")
 arg_parser_.add_argument("--pdf_file_out", type=str, help="filename to plot")
 arg_parser_.add_argument("sec", type=str, help="sec_argument")
 cmd_call_args_ = arg_parser_.parse_args()
 if cmd_call_args_.pdf_file_out is not None:
-  print (cmd_call_args_.pdf_file_out)
+  print(cmd_call_args_.pdf_file_out)
 else:
   cmd_call_args_.pdf_file_out = "out.pdf"
 if cmd_call_args_.sec != 0:
-  print (cmd_call_args_.sec)
+  print(cmd_call_args_.sec)
 
-#===============
+
+# ===============
 # a method
 def main_method():
   """sample main method"""
-  #exec_sample_plot_(cmd_call_args_.pdf_file_out)
+  # exec_sample_plot_(cmd_call_args_.pdf_file_out)
   plot_concarve_(cmd_call_args_.pdf_file_out)
 
-#===============
+
+# ===============
 # do work and call a methode
 main_method()
 
-#terminate program
+# terminate program
 sys.exit()
